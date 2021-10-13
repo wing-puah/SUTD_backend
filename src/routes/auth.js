7c55d4b7ae795b7432761e353beadafcbd004da3
wing-puah <puahhuiying@gmail.com>
2021-10-13 10:42:28 +0800

    wrap routes in error

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,24 +5,32 @@ module.exports = (service) => {
   const router = express.Router();
 
   router.post('/register', async (req, res, next) => {
-    const { username, password } = req.body;
+    try {
+      const { username, password } = req.body;
 
-    const token = await service.registerUser(username, password);
-    if (token) {
-      queueNewUserEmail(username);
-      res.send({ token: token });
-    } else {
-      res.status(400).send(`Username ${username} already exists`);
+      const token = await service.registerUser(username, password);
+      if (token) {
+        queueNewUserEmail(username);
+        res.send({ token: token });
+      } else {
+        res.status(400).send(`Username ${username} already exists`);
+      }
+    } catch (error) {
+      next(error);
     }
   });
 
   router.post('/login', async (req, res, next) => {
-    const { username, password } = req.body;
-    const token = await service.loginUser(username, password);
-    if (token) {
-      res.send({ token: token });
-    } else {
-      res.status(400).send('Invalid login credentials');
+    try {
+      const { username, password } = req.body;
+      const token = await service.loginUser(username, password);
+      if (token) {
+        res.send({ token: token });
+      } else {
+        res.status(400).send('Invalid login credentials');
+      }
+    } catch (error) {
+      next(error);
     }
   });
 
